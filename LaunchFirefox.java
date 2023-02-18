381beb5fd513fa8fdb62e0acc0ccb6c92c3263ff
Pournima Ghanmode <56918457+pournimag6@users.noreply.github.com>
2023-02-18 20:05:24 +0530

    Add files via upload

---
+++ LaunchFirefox.java
@@ -0,0 +1,12 @@
+package basics;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchFirefox {
+
+	public static void main(String[] args) {
+		new FirefoxDriver(); 
+
+	}
+
+}
