044db581f41d8f438189b7fe486ae4ebf298b2c7
Pournima Ghanmode <56918457+pournimag6@users.noreply.github.com>
2023-02-22 09:59:33 +0530

    Update LaunchChrome.java

--- LaunchChrome.java
+++ LaunchChrome.java
@@ -4,7 +4,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		new ChromeDriver();
 
 	}
